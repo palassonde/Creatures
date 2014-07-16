79882837a1d1862ea98b312b55ab47856fe0b579
palassonde <pa.lassonde@gmail.com>
2014-07-15 22:24:18 -0400

    creation du projet
    projet eclipse echangé sur git

---
+++ src/tools/Creature.java
@@ -0,0 +1,19 @@
+package tools;
+
+public class Creature {
+	
+	int hp;
+	int attack;
+	
+	
+	public Creature(){
+		hp = 10;
+		attack = 0;
+	}
+	
+	public Creature(int a, int b){
+		hp = a;
+		attack = b;
+	}
+
+}
